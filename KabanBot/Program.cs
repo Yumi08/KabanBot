c42231f8974f25f0a36334fd13769210a509e145
Yumi Ta <34748443+Yumi08@users.noreply.github.com>
2018-05-16 10:59:53 -0400

    Add Unity container.
    Implemented basic Unity container and registered IDataStorage.

--- KabanBot/Program.cs
+++ KabanBot/Program.cs
@@ -1,12 +1,10 @@
-﻿using System;
-
-namespace KabanBot
+﻿namespace KabanBot
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+	    static void Main(string[] args)
+	    {
+			Unity.RegisterTypes(); // To get it out of the way
+	    }
     }
 }
